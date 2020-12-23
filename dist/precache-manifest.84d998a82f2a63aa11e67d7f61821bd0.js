7cc6713a7fba4b05af6e66280d7517d74bdb7d23
galthran-wq <56535438+galthran-wq@users.noreply.github.com>
2020-12-23 15:39:51 +0300

    deployment

---
+++ dist/precache-manifest.84d998a82f2a63aa11e67d7f61821bd0.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9949387c6179e2dc4c675134a7b7935",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "be997d5226b992ffad34816870c6b7aa",
+    "url": "/img/theme/team-2-800x800.jpg"
+  },
+  {
+    "revision": "657ff0e1706ee9b07b4d",
+    "url": "/js/chunk-vendors.01793db3.js"
+  },
+  {
+    "revision": "54e3f3c414bd8e7234bae3ee3be950e5",
+    "url": "/img/theme/team-3-800x800.jpg"
+  },
+  {
+    "revision": "66618a418175ddf2ac8c47a241d327a8",
+    "url": "/img/theme/team-4-800x800.jpg"
+  },
+  {
+    "revision": "eafc547d837f6ba97fc6",
+    "url": "/js/app.c6f1f41c.js"
+  },
+  {
+    "revision": "794996aca77bfbb816294717e7f58458",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e55eab46b5fcfc4a7a0b27cb07c8888",
+    "url": "/img/brand/github-white-slim.png"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/img/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "fd4a34d026fb9e0f4867188d47b11ba8",
+    "url": "/img/theme/img-1-1200x1000.jpg"
+  },
+  {
+    "revision": "7789b5bfa57722dd8916b1b9ff1b1d37",
+    "url": "/img/theme/img-2-1200x1000.jpg"
+  },
+  {
+    "revision": "edc7106b21ec12e57022b2ebd534cd2d",
+    "url": "/img/theme/team-1-800x800.jpg"
+  },
+  {
+    "revision": "dc49ad52655e1d9d0552c026db3ef688",
+    "url": "/img/theme/landing.jpg"
+  },
+  {
+    "revision": "20d702b83a06bdb2ea71c4c0cb9a7a56",
+    "url": "/img/theme/profile.jpg"
+  },
+  {
+    "revision": "974088a1931e40895bac6db119c62448",
+    "url": "/img/theme/promo-1.png"
+  },
+  {
+    "revision": "99542265acb92b0398b20ebe79935465",
+    "url": "/img/label/silderei.png"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "d599f235a2c45842f421a566f25d4299",
+    "url": "/img/label/logo-removebg-preview.png"
+  },
+  {
+    "revision": "4bb165f8de4f5e081a72237e0a0c1293",
+    "url": "/img/label/love-removebg-preview.png"
+  },
+  {
+    "revision": "8f855c90b048889420586259f323cd3d",
+    "url": "/img/label/OCT-19_HFG-Website-Homepage-Images-_2500x842_4.jpg"
+  },
+  {
+    "revision": "d599f235a2c45842f421a566f25d4299",
+    "url": "/img/logo-removebg-preview.d599f235.png"
+  },
+  {
+    "revision": "19abdbb4f2c962a0b3c3c59b529f986d",
+    "url": "/img/label/love.jpg"
+  },
+  {
+    "revision": "8b9f0c345afc99f941f5160b3792e862",
+    "url": "/img/label/silderei-removebg-preview.png"
+  },
+  {
+    "revision": "4bb165f8de4f5e081a72237e0a0c1293",
+    "url": "/img/love-removebg-preview.4bb165f8.png"
+  },
+  {
+    "revision": "594b1ee1d95ada356eaad078e9217932",
+    "url": "/img/ill/ill-2.svg"
+  },
+  {
+    "revision": "8829f84ad994d707b8c6b499f99495a9",
+    "url": "/img/label/logo.jpg"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/fonts/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "c85c75275c0a0a617f9e5accc2700908",
+    "url": "/img/brand/creativetim-white-slim.png"
+  },
+  {
+    "revision": "8f855c90b048889420586259f323cd3d",
+    "url": "/img/OCT-19_HFG-Website-Homepage-Images-_2500x842_4.8f855c90.jpg"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "6fafe4baca9d50d61a898c84ade7afa3",
+    "url": "/img/brand/white.png"
+  },
+  {
+    "revision": "b9949387c6179e2dc4c675134a7b7935",
+    "url": "/img/brand/favicon.png"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/fonts/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "185288d13ed8e9d745bd279ea34667bf",
+    "url": "/img/brand/blue.png"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/fonts/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/fonts/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "eafc547d837f6ba97fc6",
+    "url": "/css/app.ba73e596.css"
+  }
+];
